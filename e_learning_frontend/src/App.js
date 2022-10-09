eea4d96d1c513e522bc7fe38b9e9a2d8489f4a7c
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-09 23:14:05 +0300

    Frontend:get all courses and load them on website

--- e_learning_frontend/src/App.js
+++ e_learning_frontend/src/App.js
@@ -2,15 +2,31 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Student from "./components/Student";
+import e_learning from "./scripts";
 
-function App() { 
+function App() {
+  const get_courses = async (setError) => {
+    const url = `${e_learning.baseUrl}get_enrolled?token=${localStorage.getItem(
+      "access_token"
+    )}`;
+    const res = await e_learning.getAPI(url);
+    if (res.status && res.status === 200) {
+      return res.data;
+    } else {
+      setError(true);
+    }
+  };
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/"} element={<Login    />} />
-        <Route path={"/login"} element={<Login    />} />
+        <Route path={"/"} element={<Login />} />
+        <Route path={"/login"} element={<Login />} />
         {/* <Route path={"/login/error"} element={<Login wrong={true} />} /> */}
-        <Route path={"/Student"} element={<Student  />} />
+        <Route
+          path={"/Student"}
+          element={<Student get_courses={get_courses} />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- e_learning_frontend/src/components/Student.jsx
+++ e_learning_frontend/src/components/Student.jsx
@@ -4,25 +4,46 @@ import Course from "./Course";
 import courseImg from "../assets/course.png";
 import userImg from "../assets/user.png";
 import e_learning from "../scripts";
+import { useEffect, useState } from "react";
 
-
-
+function Student({ get_courses }) {
+  const [error, setError] = useState(false);
+  const [Courses, setCourses] = useState([]);
+  const { avatar, name, type_id, _id } = JSON.parse(
+    localStorage.getItem("user_info")
+  );
+  useEffect(() => {
+    const get = async () => {
+      const res = await get_courses(setError);
+      setCourses(res.result);
+    };
+    get();
+  }, []);
 
   return (
     <>
-      <Header avatar={avatar}  name={name}/>
+      <Header avatar={avatar} name={name} />
+      {error && <p className="error">Some Thing is Wrong 🤨😥</p>}
       <main className="student-main">
-        <Course
-          is_std={true}
-          instructors={"Ali"}
-          course_nb={"M1101"}
-          img_instructors={userImg}
-          img_course={courseImg}
-        /> 
+        {Courses.map((course, i) => {
+          return (
+            <Course
+              key={i}
+              is_std={true}
+              instructors={course.assign_to}
+              course_nb={course.code}
+              img_instructors={
+                course.image_path !== "NA" ? userImg : course.image_path
+              }
+              img_course={
+                course.image_path !== "NA" ? courseImg : course.image_path
+              }
+            />
+          );
+        })}
       </main>
       <Footer />
     </>
   );
 }
-
 export default Student;
