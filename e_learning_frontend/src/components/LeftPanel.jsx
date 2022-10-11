c9fe191bef22c85054a5b5a894e89738a6fc10e1
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-11 16:58:14 +0300

    Frontend:Styling LeftPanel aside for admin page

--- e_learning_frontend/src/components/LeftPanel.jsx
+++ e_learning_frontend/src/components/LeftPanel.jsx
@@ -8,7 +8,20 @@ function LeftPanel() {
   const navigate = useNavigate();
   return (
     <aside className="left-panel">
-      
+      <div className="user-info">
+        <div className="user-img">
+          <img src={avatar !== "NA" ? avatar : userImg} alt="userImg" />
+        </div>
+        <p className="user-name">{name}</p>
+      </div>
+      <div className="btn-container">
+        <button className="btn">Students</button>
+        <button className="btn">Instructors</button>
+        <button className="btn">Courses</button>
+      </div>
+      <button className="btn btn-logout" onClick={() => e_learning.logout(navigate)}>
+        Logout
+      </button>
     </aside>
   );
 }
